bfef83dc2d4f30e7b3d47fc66ae37d7b8305361f
Dexter Lowe <feldoh10@gmail.com>
2023-01-20 01:30:04 +0000

    Productionise VPE_Ranger path

--- 1.4/Source/VPERanger_DefOf.cs
+++ 1.4/Source/VPERanger_DefOf.cs
@@ -6,10 +6,14 @@ namespace VPE_Ranger
     [DefOf]
     internal static class VPERanger_DefOf
     {
+#pragma warning disable CS0649
         public static HediffDef MakaiRanger_BuffTwo;
         public static HediffDef MakaiRanger_BuffThree;
         public static HediffDef MakaiRanger_BuffFour;
         public static HediffDef MakaiRanger_BuffFour_Mark;
         public static HediffDef MakaiRanger_BuffFour_Speed;
+#pragma warning restore CS0649
+
+        static VPERanger_DefOf() => DefOfHelper.EnsureInitializedInCtor(typeof(VPERanger_DefOf));
     }
 }
