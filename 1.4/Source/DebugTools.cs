a865c81b03005d2ef8b4f12f70c02c4ad10d106e
Dexter Lowe <feldoh10@gmail.com>
2024-03-09 22:41:54 +0000

    Update DebugTools.cs

--- 1.4/Source/DebugTools.cs
+++ 1.4/Source/DebugTools.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -10,9 +9,6 @@ namespace MrSamuelStreamer.RPGAdventureFlavourPack;
 
 public class DebugTools
 {
-    // foreach (CustomXenotype customXenotype in Current.Game.customXenotypeDatabase.customXenotypes)
-    // <nameMaker>${p.genes.CustomXenotype?.}</nameMaker>
-    //<chanceToUseNameMaker>1</chanceToUseNameMaker>
     [DebugAction("Pawns", "Print Xenotype", false, false, true, 0, false, actionType = DebugActionType.ToolMapForPawns, allowedGameStates = AllowedGameStates.PlayingOnMap)]
     private static void PrintXenotypeForPawn(Pawn p)
     {
@@ -20,23 +16,23 @@ public class DebugTools
             return;
 
         string blob = $"""
-                      <?xml version="1.0" encoding="utf-8" ?>
-                      <Defs>
-                      
-                          <XenotypeDef>
-                              <defName>{p.genes.xenotypeName}</defName>
-                              <label>{p.genes.xenotypeName}</label>
-                              <description>{p.genes.XenotypeDescShort}</description>
-                              <descriptionShort>{p.genes.XenotypeDescShort}</descriptionShort>
-                              <iconPath>{p.genes.Xenotype.iconPath}</iconPath>
-                              <inheritable>{p.genes.CustomXenotype?.inheritable ?? p.genes.Xenotype.inheritable}</inheritable>
-                              <genes>
-                              {p.genes.Xenogenes.Select(g => geneLine(g.def)).ToLineList()}
-                              </genes>
-                          </XenotypeDef>
-                      
-                      </Defs>
-                      """;
+                       <?xml version="1.0" encoding="utf-8" ?>
+                       <Defs>
+                       
+                           <XenotypeDef>
+                               <defName>{p.genes.xenotypeName}</defName>
+                               <label>{p.genes.xenotypeName}</label>
+                               <description>{p.genes.XenotypeDescShort}</description>
+                               <descriptionShort>{p.genes.XenotypeDescShort}</descriptionShort>
+                               <iconPath>{p.genes.Xenotype.iconPath}</iconPath>
+                               <inheritable>{p.genes.CustomXenotype?.inheritable ?? p.genes.Xenotype.inheritable}</inheritable>
+                               <genes>
+                               {p.genes.Xenogenes.Select(g => geneLine(g.def)).ToLineList()}
+                               </genes>
+                           </XenotypeDef>
+
+                       </Defs>
+                       """;
 
         Log.Message(blob);
 
@@ -47,13 +43,13 @@ public class DebugTools
     private static void PrintXenotypes()
     {
         string blob = $"""
-                      <?xml version="1.0" encoding="utf-8" ?>
-                      <Defs>
-                      
-                      {Current.Game.customXenotypeDatabase.customXenotypes.Select(XenotypeBlob).ToLineList()}
-                      
-                      </Defs>
-                      """;
+                       <?xml version="1.0" encoding="utf-8" ?>
+                       <Defs>
+
+                       {Current.Game.customXenotypeDatabase.customXenotypes.Select(XenotypeBlob).ToLineList()}
+
+                       </Defs>
+                       """;
 
         Log.Message(blob);
     }
@@ -86,16 +82,15 @@ public class DebugTools
     }
 
 
-
     public static void PrintXenotype(CustomXenotype xenotype, string absFilePath)
     {
         try
         {
             xenotype.fileName = Path.GetFileNameWithoutExtension(absFilePath);
-            SafeSaver.Save(absFilePath, "savedXenotype", (Action) (() =>
+            SafeSaver.Save(absFilePath, "savedXenotype", (Action)(() =>
             {
                 ScribeMetaHeaderUtility.WriteMetaHeader();
-                Scribe_Deep.Look<CustomXenotype>(ref xenotype, nameof (xenotype));
+                Scribe_Deep.Look<CustomXenotype>(ref xenotype, nameof(xenotype));
             }));
         }
         catch (Exception ex)
@@ -104,4 +99,3 @@ public class DebugTools
         }
     }
 }
-
