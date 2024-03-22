dd12a1a80e63616331db064f7d4f744e5692d25d
Dexter Lowe <feldoh10@gmail.com>
2024-03-22 17:56:54 +0000

    Batch file for lazy streamers

---
+++ release.bat
@@ -0,0 +1,6 @@
+dotnet build .\1.4\Source\RPGAdventureFlavourPack.sln /p:Configuration=Release
+@ECHO OFF
+ECHO Building RPGAdventureFlavourPack 1.4 - Complete
+
+ECHO Press any key to exit...
+PAUSE > NUL
