ffe2cee5640f3fd68792f8c28a87c77ddf00f0d4
Dexter Lowe <feldoh10@gmail.com>
2024-03-22 03:07:07 +0000

    #79 Extra Deep Infestation event

---
+++ 1.4/Source/Building_EnragingWorkTable.cs
@@ -0,0 +1,21 @@
+﻿using RimWorld;
+using Verse;
+
+namespace MrSamuelStreamer.RPGAdventureFlavourPack;
+
+public class Building_EnragingWorkTable : Building_WorkTable
+{
+    public CompCausesEnrage enrageComp;
+
+    public override void UsedThisTick()
+    {
+        base.UsedThisTick();
+        enrageComp?.Notify_UsedThisTick();
+    }
+
+    public override void SpawnSetup(Map map, bool respawningAfterLoad)
+    {
+        base.SpawnSetup(map, respawningAfterLoad);
+        enrageComp = GetComp<CompCausesEnrage>();
+    }
+}
